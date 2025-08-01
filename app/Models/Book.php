16517284f190479809cfe46e266cf17b2d76f1b2
Raymond Lay <77760136+raymondlay023@users.noreply.github.com>
2025-08-01 11:04:15 +0700

    model relations

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use App\Enums\BookLanguage;
 use App\Enums\BookStatus;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Book extends Model
 {
@@ -31,5 +34,26 @@ class Book extends Model
             'language' => BookLanguage::class,
             'status' => BookStatus::class,
         ];
+    }
+
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function stock(): HasOne
+    {
+        return $this->hasOne(Stock::class);
+    }
+
+    public function loans(): HasMany
+    {
+        return $this->hasMany(Loan::class);
+    }
+
+    public function publisher(): BelongsTo
+    {
+        return $this->belongsTo(Publisher::class);
     }
 }
