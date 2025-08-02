96b16dab2d0ba8beaf09a32b98020f964750c38b
Raymond Lay <raymondlay023@gmail.com>
2025-08-03 00:41:53 +0700

    modify and add some components

---
+++ resources/js/Components/NavLinkResponsive.jsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+
+export default function NavLinkResponsive({
+    active = false,
+    url = "#",
+    title,
+    icon: Icon,
+    ...props
+}) {
+    return (
+        <Link
+            {...props}
+            href={url}
+            className={cn(
+                active
+                    ? "bg-gradient-to-r from-orange-400 via-orange-500 to-orange-500 font-semibold text-white hover:text-white"
+                    : "text-muted-foreground hover:text-orange-500",
+                "flex items-center gap-3 rounded-lg p-2 font-medium transition-all"
+            )}
+        ></Link>
+    );
+}
