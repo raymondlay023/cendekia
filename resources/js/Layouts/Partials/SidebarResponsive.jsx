6e13c7d68a5100266e31f857546051792f2217e6
Raymond Lay <raymondlay023@gmail.com>
2025-11-22 12:45:50 +0700

    Enhance profile management UI and functionality with new components and layout adjustments

--- resources/js/Layouts/Partials/SidebarResponsive.jsx
+++ resources/js/Layouts/Partials/SidebarResponsive.jsx
@@ -95,7 +95,12 @@ export default function SidebarResponsive({ url, auth }) {
                     title="Pengumuman"
                     icon={IconAlertCircle}
                 />
-                <NavLink url={route('profile.edit')} title="Profile" icon={IconUser} />
+                <NavLink
+                    url={route('profile.edit')}
+                    active={url.startsWith('/admin/profile')}
+                    title="Profile"
+                    icon={IconUser}
+                />
                 <NavLink
                     url={route('logout')}
                     title="Logout"
