aed4dd36212c70834126e395259e42181cebdb59
Raymond Lay <raymondlay023@gmail.com>
2025-11-29 14:05:47 +0700

    feat: implement user role assignment functionality with CRUD operations

--- resources/js/Layouts/Partials/SidebarResponsive.jsx
+++ resources/js/Layouts/Partials/SidebarResponsive.jsx
@@ -88,7 +88,12 @@ export default function SidebarResponsive({ url, auth }) {
                     title="Tetapkan Izin"
                     icon={IconKeyframe}
                 />
-                <NavLink url="#" title="Tetapkan Peran" icon={IconLayoutKanban} />
+                <NavLink
+                    url={route('admin.assign-users.index')}
+                    active={url.startsWith('/admin/assign-users')}
+                    title="Tetapkan Peran"
+                    icon={IconLayoutKanban}
+                />
                 <NavLink url="#" title="Akses Rute" icon={IconRoute} />
                 <div className="px-3 py-2 text-sm font-semibold text-foreground">Transaksi</div>
                 <NavLink
