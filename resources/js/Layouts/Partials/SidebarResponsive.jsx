30cbacad8bced161698286774d49bb4f2a74694b
Raymond Lay <77760136+raymondlay023@users.noreply.github.com>
2025-12-05 11:28:20 +0700

    add route to sidebar

--- resources/js/Layouts/Partials/SidebarResponsive.jsx
+++ resources/js/Layouts/Partials/SidebarResponsive.jsx
@@ -94,7 +94,12 @@ export default function SidebarResponsive({ url, auth }) {
                     title="Tetapkan Peran"
                     icon={IconLayoutKanban}
                 />
-                <NavLink url="#" title="Akses Rute" icon={IconRoute} />
+                <NavLink
+                    url={route('admin.route-accesses.index')}
+                    active={url.startsWith('/admin/route-accesses')}
+                    title="Akses Rute"
+                    icon={IconRoute}
+                />
                 <div className="px-3 py-2 text-sm font-semibold text-foreground">Transaksi</div>
                 <NavLink
                     url={route('admin.loans.index')}
